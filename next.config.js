de5fbdb786c4439c8761634ae4b90bf66de84441
Christopher Miller <admin@mctherealm.net>
2019-11-15 16:27:12 -0500

    Fixed font awesome style including

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const sass = require('@zeit/next-sass')
+const css = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withFonts = require('next-fonts');
@@ -6,6 +7,7 @@ const withFonts = require('next-fonts');
 module.exports = withPlugins(
   [
     [sass, {}],
+    [css, {}],
     [optimizedImages, {
       handleImages: ['jpeg', 'png'],
       optimizeImages: true
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import App from 'next/app'
 import Head from 'next/head'
 import React from 'react'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
