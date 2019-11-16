5d92ebe65496f71bbb044a386e5c6410777f1add
ConnorJC <ccatlett2000@gmail.com>
2019-11-16 17:47:22 -0500

    Move favicon to public/ (static/ is deprecated in next.js)

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class Doc extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="/static/favicon.png" />
+          <link rel="shortcut icon" href="/favicon.png" />
           <meta name="Description" content="The Association of Computer Engineers is a Professional Organization at the University of Florida that caters to Computer Science, Computer Engineering, and Electrical Engineering Majors." />
         </Head>
         <body>
