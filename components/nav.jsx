525940f798958a558876b8ca021196d5f1ab66ed
Christopher Miller <admin@mctherealm.net>
2020-09-08 12:17:33 -0400

    Updated Nav Links

--- components/nav.jsx
+++ components/nav.jsx
@@ -21,8 +21,8 @@ function Header() {
             <Nav id="navbar" className="ml-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#schedule">Schedule</Nav.Link>
-              <Nav.Link href="">Join</Nav.Link>
+              <Nav.Link href="https://events.uf-ace.com/">Events</Nav.Link>
+              <Nav.Link href="https://uf-ace.slack.com">Join Slack</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
